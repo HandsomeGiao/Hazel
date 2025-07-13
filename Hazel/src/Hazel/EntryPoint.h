f0eed8452d7ac26ac6bb333a55faa30d6fd0ff08
FishTofu <giaogiao521@163.com>
2025-07-13 21:48:43 +0800

    log system

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_INFO("Hazel Engine Initialized!");
+	HZ_TRACE("Application Starting...");
+
 	auto app = Hazel::CreateApplication();
 	app->run();
 	delete app;
