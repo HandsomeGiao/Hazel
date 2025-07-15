ba2fcd40255d5683629de64cc8c38b2601102509
FishTofu <giaogiao521@163.com>
2025-07-15 09:13:57 +0800

    glfw and first window

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,9 +1,12 @@
 #include "hzpch.h"
 
 #include "Application.h"
+#include <GLFW/glfw3.h>
+
 namespace Hazel {
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -12,8 +15,10 @@ namespace Hazel {
 
 	void Application::run()
 	{
-		while (true) {
-
+		while (m_Running) {
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
 	}
 }
